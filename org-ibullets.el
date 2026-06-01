293d30cfaa029f86383220d8bc703894886af549
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-06-01 12:25:55 -0400

    Release: 1.0.1

--- org-ibullets.el
+++ org-ibullets.el
@@ -6,7 +6,7 @@
 ;; Original Author: Evgkeni Sampelnikof (Sabof)
 ;; Package-Requires: ((emacs "25.1"))
 ;; Keywords: maint
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
 ;; This file is NOT part of GNU Emacs.
